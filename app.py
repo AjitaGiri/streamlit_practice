63a37a417cef3ca82f626c6cb6afa65c25d854f3
AjitaGiri <ajitagiri07@gmail.com>
2026-04-22 10:45:15 +0545

    basic concepts in streamlit

--- app.py
+++ app.py
@@ -4,6 +4,3 @@ import streamlit as st
 st.title("my first app")
 st.subheader("streamlit app")
 st.text("hello world")
-
-# selectbox
-language= st.selectbox("Your Favourite Programming Language:",['Python','Java','JavaScript','C'])
